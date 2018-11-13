c9dc51767cf05d91b6a6a01258aa594cfe14e7e5
Enrique Ramirez <enrique.ramirez35@gmail.com>
2018-11-12 18:48:49 -0700

    Initial app commit

---
+++ src/TimeHeader/TimeHeader.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './TimeHeader.css'
+export class TimeHeader extends Component {
+  render() {
+    return (
+      <div className="col-md-8 offset-md-4 time-header">
+        <strong>Today is: {this.getDate()}</strong>
+      </div>
+    );
+  }
+
+  getDate() {
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today = new Date();
+    return today.toLocaleDateString("en-US", options);
+  }
+}
