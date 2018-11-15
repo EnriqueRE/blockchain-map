547ad2c6597e5c7251a788b90b41f03aedfe3a8c
Enrique Ramirez <enrique.ramirez35@gmail.com>
2018-11-15 16:33:51 -0700

    Added builds

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { TimeHeader } from './TimeHeader/TimeHeader';
 import { Map } from './Map/Map';
 import { Timeline } from './Timeline/Timeline';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App container-fluid">
+      <div className="App container">
         <div className="row">
           <div className="col-md-12">
             <TimeHeader />
           </div>{' '}
         </div>{' '}
-        <div className="row"></div>{' '}
+        <div className="row"> </div>{' '}
         <div className="row">
-          <div className="col col-sm-12, col-md-6, col-lg-6"> <Timeline/></div>{' '}
+          <div className="col col-sm-12, col-md-6, col-lg-6">
+            {' '}
+            <Timeline />{' '}
+          </div>{' '}
           <div className="col col-sm-12, col-md-6, col-lg-6">
             {' '}
             <Map />{' '}
--- src/Timeline/Timeline.jsx
+++ src/Timeline/Timeline.jsx
@@ -1,47 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Timeline.css';
-export class Timeline extends Component{
-  render(){
-    return(
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTruck,
+	faExclamationTriangle,
+	faFileAlt
+} from '@fortawesome/free-solid-svg-icons';
+export class Timeline extends Component {
+  render() {
+    return (
       <ul className="timeline">
-      <li>
-				<div className="shipment-container">
-					<p className="shipment-status">Shipment Delivered</p>
-					<p className="shipment-date">{this.getDate()}</p>
-					<p className="shipment-details">Shipment 35191 delivered. Serial#AS475-31001</p>
-					<p className="shipment-carrier">Gamma Carrier</p>
-				</div>
-				</li>
+        <li>
+          <div className="shipment-container">
+            <p className="shipment-status">Shipment Delivered</p>
+            <p className="shipment-date">{this.getDate()}</p>
+            <p className="shipment-details">
+              Shipment 35191 delivered. Serial#AS475-31001
+            </p>
+            <p className="shipment-carrier">Gamma Carrier</p>
+          </div>
+        </li>
+        <li>
+          <div className="shipment-container container-warning">
+            <p className="shipment-status">Shipment Delivered</p>
+            <p className="shipment-date">{this.getDate()}</p>
+            <p className="shipment-details">Temperature out of range.</p>
+            <p className="shipment-carrier">Gamma Carrier</p>
+            <div className="transaction-warning">
+              <h3>
+                38.2°F <FontAwesomeIcon icon={faTruck} />
+              </h3>
+              <strong>
+                <FontAwesomeIcon icon={faExclamationTriangle} /> Transaction
+                Warning
+              </strong>
+            </div>
+          </div>
+        </li>
 				<li>
-				<div className="shipment-container">
-					<p className="shipment-status">Shipment Delivered</p>
-					<p className="shipment-date">{this.getDate()}</p>
-					<p className="shipment-details">Shipment 35191 delivered. Serial#AS475-31001</p>
-					<p className="shipment-carrier">Gamma Carrier</p>
-				</div>
-				</li>
-				<li>
-				<div className="shipment-container">
-					<p className="shipment-status">Shipment Delivered</p>
-					<p className="shipment-date">{this.getDate()}</p>
-					<p className="shipment-details">Shipment 35191 delivered. Serial#AS475-31001</p>
-					<p className="shipment-carrier">Gamma Carrier</p>
-				</div>
-				</li>
-				<li>
-				<div className="shipment-container">
-					<p className="shipment-status">Shipment Delivered</p>
-					<p className="shipment-date">{this.getDate()}</p>
-					<p className="shipment-details">Shipment 35191 delivered. Serial#AS475-31001</p>
-					<p className="shipment-carrier">Gamma Carrier</p>
-				</div>
-				</li>
+          <div className="shipment-container">
+            <p className="shipment-status">Shipment Departed <FontAwesomeIcon icon={faFileAlt} className="status-icon"/></p>
+            <p className="shipment-date">{this.getDate()}</p>
+            <p className="shipment-details">
+              Shipment 35191 delivered. Serial#AS475-31001
+            </p>
+            <p className="shipment-carrier">Gamma Carrier</p>
+          </div>
+        </li>
+				<li className="disabled">
+          <div className="shipment-container">
+            <p className="shipment-status">Shipment Delivered</p>
+            <p className="shipment-date">{this.getDate()}</p>
+            <p className="shipment-details">
+              Shipment 35191 delivered. Serial#AS475-31001
+            </p>
+            <p className="shipment-carrier">Gamma Carrier</p>
+          </div>
+        </li>
       </ul>
     );
   }
- getDate(){
-	var options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric',  hour:'numeric', minute:'numeric'};
-	let today = new Date();
-	return today.toLocaleDateString("en-US", options);
- }
+  getDate() {
+    var options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    };
+    let today = new Date();
+    return today.toLocaleDateString('en-US', options);
+  }
 }
